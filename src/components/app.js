fbe0d84db98f62bd1b729a21f5f6f589cb88d820
Dmitry Sichkar <dmmeteo@gmail.com>
2017-11-07 00:57:08 +0200

    using State

--- src/components/app.js
+++ src/components/app.js
@@ -5,15 +5,18 @@ import Comments from './comments';
 var my_news = [
     {
         author: 'John Doe',
-        text: 'On Thursday, the fourth day...'
+        text: 'On Thursday, the fourth day...',
+        bigText: 'at four and a quarter of an hour four black-eyed black-haired devils drew a black ink drawing.'
     },
     {
         author: 'Just Harry',
-        text: 'I think that $ should be worth 5 hryvnia!'
+        text: 'I think that $ should be worth 5 hryvnia!',
+        bigText: 'And the euro 8!'
     },
     {
         author: 'Guest',
-        text: 'Is free. Download. The best site - http: // localhost: 3000'
+        text: 'Is free. Download. The best site - http: // localhost: 3000',
+        bigText: 'In fact, you just need to read a very long license agreement.'
     }
 ];
 
--- src/components/article.js
+++ src/components/article.js
@@ -2,21 +2,40 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types'; //Migrating from React.PropTypes after(v15.5)
 
 class Article extends Component {
+    constructor(props){
+       super(props);
+       this.state = {visible: false};
+    };
+
     static propTypes = {
         data: PropTypes.shape({
             author: PropTypes.string.isRequired,
-            text: PropTypes.string.isRequired
+            text: PropTypes.string.isRequired,
+            bigText: PropTypes.string.isRequired
         })
     };
 
+    readmoreClick(e){
+        e.preventDefault();
+        this.setState({visible: true}, function() {
+            alert('State is changed!');
+        });
+    }
+
     render() {
         var author = this.props.data.author,
-            text = this.props.data.text;
+            text = this.props.data.text,
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
 
         return (
             <div className="article">
                 <p className="news__author">{author}</p>
                 <p className="news__text">{text}</p>
+                <a href="#"
+                   onClick={this.readmoreClick.bind(this)}
+                   className={'news__readmore ' + (visible ? 'none':'')}>Detail...</a>
+                <p className={'news__big-text ' + (visible ? '':'none')}>{bigText}</p>
             </div>
         )
     }
