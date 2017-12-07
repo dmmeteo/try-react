b378fc1d481e78822fb8659b9eb3d9eca65cc663
Dmitry Sichkar <dmmeteo@gmail.com>
2017-12-07 18:03:04 +0200

    add simple middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,14 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
+import {ping} from './enhancers/ping';
+
 
 export default function configureStore(initialState) {
-    const store = createStore(rootReducer, initialState);
+    const store = createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(ping)
+    );
+
     return store
 }
