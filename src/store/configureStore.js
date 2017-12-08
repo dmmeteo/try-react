996a81c56368ebe394ae81be26fbd4b5eeee3423
Dmitry Sichkar <dmmeteo@gmail.com>
2017-12-08 09:23:14 +0200

    middleware array not working

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,16 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
+import createLogger from 'redux-logger';
 import {ping} from './enhancers/ping';
 
 
 export default function configureStore(initialState) {
+    const logger = createLogger();
+    const middleware = [logger, ping];
     const store = createStore(
         rootReducer,
         initialState,
-        applyMiddleware(ping)
+        applyMiddleware(...middleware)
     );
 
     return store
