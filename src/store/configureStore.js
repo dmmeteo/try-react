ae2ef99a3936235894dd1b808771d1a44d708d26
Dmitry Sichkar <dmmeteo@gmail.com>
2017-12-12 00:57:48 +0200

    fixed redux logger & fin middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
-import createLogger from 'redux-logger';
+import logger from 'redux-logger';
 import {ping} from './enhancers/ping';
 
-
+const middleware = [logger, ping];
 export default function configureStore(initialState) {
-    const logger = createLogger();
-    const middleware = [logger, ping];
     const store = createStore(
         rootReducer,
         initialState,
